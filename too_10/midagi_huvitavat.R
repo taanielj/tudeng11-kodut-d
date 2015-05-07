27f49b2cbe20acd829728ad3eda6a23881565ce3
tanni0 <taaniel.jakobson@gmail.com>
2015-05-07 11:46:15 +0300

    midagi huvitavat

---
+++ too_10/midagi_huvitavat.R
@@ -0,0 +1,5 @@
+#butterfly curve
+t = seq(-1000,1000,0.01)
+x = sin(t)*(exp(1)**cos(t)-2*cos(4*t)-sin(t/12)*sin(t/12)*sin(t/12)*sin(t/12)*sin(t/12))
+y = cos(t)*(exp(1)**cos(t)-2*cos(4*t)-sin(t/12)*sin(t/12)*sin(t/12)*sin(t/12)*sin(t/12))
+plot(x,y, type = "l")
