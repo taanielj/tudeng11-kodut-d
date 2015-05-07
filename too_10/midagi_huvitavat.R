27b791836a81d3219106a2cadbe78e9d1a1e8b21
tanni0 <taaniel.jakobson@gmail.com>
2015-05-07 11:51:52 +0300

    Update midagi_huvitavat.R

--- too_10/midagi_huvitavat.R
+++ too_10/midagi_huvitavat.R
@@ -2,4 +2,4 @@
 t = seq(-1000,1000,0.01)
 x = sin(t)*(exp(1)**cos(t)-2*cos(4*t)-sin(t/12)*sin(t/12)*sin(t/12)*sin(t/12)*sin(t/12))
 y = cos(t)*(exp(1)**cos(t)-2*cos(4*t)-sin(t/12)*sin(t/12)*sin(t/12)*sin(t/12)*sin(t/12))
-plot(x,y, type = "l")
+plot(x,y, cex = 0.1)
